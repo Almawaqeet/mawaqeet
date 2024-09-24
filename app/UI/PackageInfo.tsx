b5c0cbb4dcf8e8be452ad33a4b2564e94121fb37
profstrategy <ojobalaro2018@gmail.com>
2024-09-24 16:22:24 -0700

    made changes

--- app/UI/PackageInfo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PackageInfo = () => {
-  return (
-    <div>PackageInfo</div>
-  )
-}
-
-export default PackageInfo
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { MbisProvider } from "./libs/hooks/useContextProvider";
 import HomeV1 from "./UI/HomeV1";
-import Homes from "./pages/Home";
 import Navbar from "./UI/Navbar";
 import HomeV2 from "./UI/HomeV2";
+import Homes from "./pages/Home";
 
 export default function Home() {
   return (
