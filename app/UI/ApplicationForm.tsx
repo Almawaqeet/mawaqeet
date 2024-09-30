5878784f26f569db326b64f88a960538e7fcfa63
profstrategy <ojobalaro2018@gmail.com>
2024-09-30 13:06:22 -0700

    fix context location error

---
+++ app/UI/ApplicationForm.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React from 'react';
+import { whiteSpaces } from '../libs/utilities/GlobalSpaces';
+import { appForm } from '../contents/payment';
+import { MdArrowRightAlt } from "react-icons/md";
+
+
+import { useAppInfo } from '../libs/hooks/useAppInfo';
+import Footer from './Footer';
+import ComplementNum from './ComplementNum';
+import PaymentPage, { PaymentPageSteps } from '../registration-form/components/PaymentPage';
+
+
+const app = () => {
+    const { selectedNumber } = useAppInfo();
+    return (
+        <div>
+            <section className='max-w-[2000px] m-auto'>
+                <main className={`${whiteSpaces.paddingX}`}>
+
+                    <div className='grid justify-stretch xmd:pb-[55px]'>
+                        <div className='grid grid-cols-3 xmd:w-full md:w-2/4 justify-center m-auto xmd:items-center'>
+                            {appForm.map((app, i) => (
+                                <div className='w-full' key={app.id}>
+                                    <span className='flex relative'>
+
+                                        <ComplementNum
+                                            className={`xmd:w-[50px] xmd:h-[50px] m-auto cursor-pointer md:grid md:mx-0 relative ${selectedNumber === i ? 'bg-hover-color text-white transition-all' : 'bg-white border-dotted border-2 border-[#333333]'}`}
+                                            position={i + 1}
+                                        />
+
+
+                                        {i !== 2 && (
+                                            <MdArrowRightAlt
+                                                className='absolute top-1/2 text-[82px] left-3/4 transform -translate-y-1/2 m-auto text-[#A0A8AD] md:left-2/4'
+
+                                            />
+                                        )}
+                                    </span>
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </main>
+
+               {selectedNumber === 0 ? <PaymentPage steps={PaymentPageSteps.ONBOARDING} /> : selectedNumber === 1 ? <PaymentPage steps={PaymentPageSteps.PACKAGE_SELECTION} /> : selectedNumber === 2 ? <PaymentPage steps={PaymentPageSteps.EXTRA_INFORMATION} /> : null}
+            </section>
+
+            <section className='xmd:mt-[152px]'>
+                <Footer />
+            </section>
+        </div>
+    );
+};
+
+export default app;
