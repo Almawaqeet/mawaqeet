2a765ba27af1f0ee400607fac2bfc303d24a4b53
profstrategy <ojobalaro2018@gmail.com>
2024-10-06 06:42:13 -0700

    clean up FE ui

--- app/UI/Profile.tsx
+++ app/UI/Profile.tsx
@@ -22,30 +22,30 @@ interface ProfileProps {
 const Profile: React.FC<ProfileProps> = ({ team }) => {
     const { id, profile_1, profile_2, profile_3, profile_4, personality } = team
     return (
-        <div className={`${whiteSpaces.paddingX} xmd:pt-64 md:pt-80 lg:pt-20 pb-[150px]`} key={id}>
+        <div className={`${whiteSpaces.paddingX} xmd:pt-44 sm:pt-60 md:pt-[22rem] lg:pt-20 pb-[150px]`} key={id}>
             <Headings type='sectionName' classname='text-center xmd:pb-11 lg:pb-20 font-dejavu'>{personality}</Headings>
             <Paragraph
                 type="global"
-                classname="font-normal xmd:leading-5 md:leading-8 xmd:tracking-[0.16px] text-team-clamp xmd:pb-8 font-dejavu text-justify lg:pb-12"
+                classname="font-normal xmd:leading-5 md:leading-8 xmd:tracking-[0.16px] text-team-clamp xmd:pb-8 font-dejavu text-left lg:pb-12"
             >
                 {profile_1}
             </Paragraph>
 
             <Paragraph
                 type="global"
-                classname="font-normal xmd:leading-5 md:leading-8 xmd:tracking-[0.16px] text-team-clamp xmd:pb-8 lg:pb-12 font-dejavu text-justify"
+                classname="font-normal xmd:leading-5 md:leading-8 xmd:tracking-[0.16px] text-team-clamp xmd:pb-8 lg:pb-12 font-dejavu text-center"
             >
                 {profile_2}
             </Paragraph>
             <Paragraph
                 type="global"
-                classname="font-normal xmd:leading-5 text-justify md:leading-8 xmd:tracking-[0.16px] text-team-clamp xmd:pb-8 lg:pb-12 font-dejavu"
+                classname="font-normal xmd:leading-5 text-center md:leading-8 xmd:tracking-[0.16px] text-team-clamp xmd:pb-8 lg:pb-12 font-dejavu"
             >
                 {profile_3}
             </Paragraph>
             <Paragraph
                 type="global"
-                classname="font-normal xmd:leading-5 text-justify md:leading-8 xmd:tracking-[0.16px] text-team-clamp font-dejavu"
+                classname="font-normal xmd:leading-5 text-center md:leading-8 xmd:tracking-[0.16px] text-team-clamp font-dejavu"
             >
                 {profile_4}
             </Paragraph>
--- app/about-us/[slug]/page.tsx
+++ app/about-us/[slug]/page.tsx
@@ -24,15 +24,15 @@ const Page: React.FC<PageProps> = ({ params }) => {
     return (
         <MbisProvider>
             <Navbar />
-            <section className='max-w-[2000px] m-auto bg-personality xmd:pt-16 lg:pt-0'>
+            <section className='max-w-[2000px] m-auto bg-personality lg:pt-0'>
                 <main className=' md:pt-0 relative lg:h-screen'>
-                    <div key={teamMember.id} className={`xmd:grid lg:grid-cols-[1fr_1fr] xmd:grid-rows-1 items-center lg:gap-16 lg:justify-between  xmd:h-[448px] md:h-[420px]  ${whiteSpaces.paddingX} `}>
+                    <div key={teamMember.id} className={`xmd:grid lg:grid-cols-[1fr_1fr] xmd:grid-rows-1 items-center lg:gap-16 lg:justify-between  xmd:h-[748px] md:h-[620px]  ${whiteSpaces.paddingX} `}>
                         <div className='xmd:gap-5 flex xmd:flex-col lg:justify-center'>
-                            <Headings type='heroHeading' classname='text-white xmd:pt-14'>{teamMember.fullname}</Headings>
+                            <Headings type='heroHeading' classname='text-white '>{teamMember.fullname}</Headings>
                             <Headings type='BrandText' classname='text-Bold-1-clamp xmd:font-normal font-dejavu text-white text-center'>{teamMember.post}</Headings>
                         </div>
-                        <div className="lg:pt-52">
-                            <Image src={teamMember.imageProfile} alt={`${teamMember.fullname}'s image`} width={100} height={100} className="object-cover m-auto xmd:absolute lg:static xmd:w-[332px] mobile:w-[400px] md:h-[500px] sm:w-[483px] md:w-[683px] rounded-3xl xmd:left-1/2 transform xmd:-translate-x-1/2 lg:translate-x-0 lg:left-0 xmd:top-2/3 sm:top-1/2 md:top-2/4 lg:top-0" sizes='w-0' />
+                        <div className="">
+                            <Image src={teamMember.imageProfile} alt={`${teamMember.fullname}'s image`} width={100} height={100} className="object-cover m-auto xmd:absolute lg:static xmd:w-[332px] mobile:w-[400px] md:h-[500px] sm:w-[483px] md:w-[683px] rounded-3xl xmd:left-1/2 transform xmd:-translate-x-1/2 lg:translate-x-0 lg:left-0 xmd:top-3/4 md:top-2/3 lg:top-0" sizes='w-0' />
                         </div>
                     </div>
                 </main>
