af231d1a0191d7995e21231c07e713d65de5fc65
profstrategy <ojobalaro2018@gmail.com>
2025-01-31 01:38:44 -0800

    reverted previous navbar changes and enhance whitespaces

--- app/(public-pages)/_components/about-page/AboutPageSection.tsx
+++ app/(public-pages)/_components/about-page/AboutPageSection.tsx
@@ -13,7 +13,7 @@ const Paragraph = dynamic(() => import('@/old-pages/utilities/Paragraph'), {
 export const AboutPageSection = () => {
   return (
     <section
-      className={`flex min-h-[30%] items-center justify-center flex-col sm:py-4 xmd:py-8 ${whiteSpaces.paddingX}`}
+      className={`flex min-h-[30%] items-center justify-center flex-col py-4 ${whiteSpaces.paddingX}`}
     >
       <div>
         <AppHeading variant="h1" className=" text-H1-text-color mb-4">
