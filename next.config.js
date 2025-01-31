09c46888834e317a2cf997dcbf2e70fb1b04af5e
Shereef Habib <sheryf.534@gmail.com>
2025-01-31 23:17:12 +0100

    feat: added the iata logo from the link section

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'api.slingacademy.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.freebiesupply.com',
+      },
     ],
   },
 };
