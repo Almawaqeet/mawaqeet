6e6d9982aa37e5381b2c48b3a069a950acd091dc
Shereef Habib <sheryf.534@gmail.com>
2025-07-02 10:00:19 +0100

    deployment fix

--- app/(onboarding)/onboarding/new-user/step-3/page.tsx
+++ app/(onboarding)/onboarding/new-user/step-3/page.tsx
@@ -5,16 +5,34 @@ import {
   QueryClient,
   dehydrate,
 } from '@tanstack/react-query';
-
+import { createServerAxiosInstance } from '@/network/server-constructor';
+import { generateBaseQueryKeyFromRoute, routes } from '@/network/routes';
 
 const StepThreeOnboarding = dynamic(
   () => import('@/app/(public-pages)/_components/onboarding/StepThree'),
   { ssr: false }
 );
 
+async function getInitialData() {
+  const queryClient = new QueryClient();
+  const route = routes.onboarding.getOnboardingPaymentAmount;
+
+  const baseQueryKey = generateBaseQueryKeyFromRoute(route);
+
+  try {
+    const data = await createServerAxiosInstance(route);
+    await queryClient.prefetchQuery({
+      queryKey: [baseQueryKey],
+      queryFn: () => data,
+    });
+    return queryClient;
+  } catch (error) {
+    console.error('Error fetching initial data:', error);
+  }
+}
 
 export default async function StepThreePage() {
-  const queryClient = new QueryClient();
+  const queryClient = (await getInitialData()) ?? new QueryClient();
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
